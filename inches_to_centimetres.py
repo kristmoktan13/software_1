c334a3c6be670b27545a43cd2eb3b835568e02aa
kristmoktan13 <kristmoktan13@gmail.com>
2026-02-01 22:31:55 +0545

    completed

--- inches_to_centimetres.py
+++
@@ -1,8 +0,0 @@
-while True:
-    inches = float(input("Enter in inches (no negative values): "))
-
-    if inches < 0:
-        break
-
-    centimeters = inches * 2.54
-    print(centimeters, "cm")
